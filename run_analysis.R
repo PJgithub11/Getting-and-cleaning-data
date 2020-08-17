6d7b3afd2d0cee5665dfdfaaada7793568da38e2
PJgithub11 <65217074+PJgithub11@users.noreply.github.com>
2020-08-17 06:11:21 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,13 @@
+
+#step 1 Merges the training and the test sets to create one data set.
+#step 2 Extracts only the measurements on the mean and standard deviation for each measurement.
+#step 3 Uses descriptive activity names to name the activities in the data set
+#step 4 Appropriately labels the data set with descriptive variable names.
+#step 5 From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 # run_analysis.R
 
-#0. prepare LIBs
+
 library(reshape2)
 
 
